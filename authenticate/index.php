e7cf3175a30d5cf4173cdabb3a0a789f9c41e185
Anony <60695456+Anonylions@users.noreply.github.com>
2021-01-02 16:24:03 -0300

    Add files via upload

--- authenticate/index.php
+++ authenticate/index.php
@@ -10,15 +10,15 @@
 <div class="container">
 	<div class="row">
 		<div class="six offset-by-three columns auth-section">
-			<h4>Sign in or Register</h4>
-			<p>Using the form below please enter your email address and password and we'll get started.</p>
+			<h4>Entrar ou Registrar</h4>
+			<p>Usando o formulário abaixo, digite seu endereço de e-mail e senha e começaremos.</p>
 			<form id="auth" action="" method="post">
 				<div class="row">
 					<div class="six columns">
 					    <input type="radio" id="radio1" name="type" value="returning_user" checked="checked">
 	    				<label class="user-type" for="radio1">
-	    					<b>Returning User</b>
-	    					<p>If you are registered before please sign in below.</p>
+	    					<b>Usuário recorrente</b>
+	    					<p>Se você já se registrou antes, faça login abaixo.</p>
 	    				</label>
     				</div>
 				
@@ -26,19 +26,19 @@
 					    <input type="radio" id="radio2" name="type" value="new_user">
 	    				<label class="user-type" for="radio2">
 	    					<b>New User</b>
-	    					<p>If you're not already signed up then you'll need to do so</p>
+	    					<p>Se você ainda não se inscreveu, você precisará fazer isso</p>
 	    				</label>
     				</div>
 				</div>
         <span id="alerts"></span>
-				<label for="email">Email Address</label>
-				<input class="u-full-width" type="email" name="email" required autocomplete="off" placeholder="you@domain.com" id="email">
+				<label for="email">Endereço de e-mail</label>
+				<input class="u-full-width" type="email" name="email" required autocomplete="off" placeholder="seu@domain.com" id="email">
 				
-				<label for="password">Password</label>
-				<input class="u-full-width" type="password" name="password" required placeholder="Keep it secure" id="password">
+				<label for="password">Senha</label>
+				<input class="u-full-width" type="password" name="password" required placeholder="****" id="password">
 				
-				<button class="button" name="submit" type="submit">Authenticate</button>
-				<button class="button" name="back" type="back" onClick="window.location.href='../';">Back</button>
+				<button class="button" name="submit" type="submit">Autenticar</button>
+				<button class="button" name="back" type="back" onClick="window.location.href='../';">De volta</button>
 			</form>
 			
 		</div>
